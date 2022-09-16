a1c4b1a391f4aa8e382f7f14e471e3cb381d8ac7
gopi <gopikoragg@gmail.com>
2022-09-16 15:09:59 +0530

    Bank System

---
+++ src/bank/management/system/MiniStatement.java
@@ -0,0 +1,86 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+
+public class MiniStatement extends JFrame implements ActionListener {
+
+    String pinnumber;
+    JButton exit;
+    MiniStatement(String pinnumber){
+
+        this.pinnumber = pinnumber;
+        setLayout(null);
+
+        JLabel mini = new JLabel();
+        add(mini);
+
+        JLabel bank = new JLabel("Indian Bank");
+        bank.setBounds(150,20,100,30);
+        add(bank);
+
+        JLabel card = new JLabel();
+        card.setBounds(20,80,300,20);
+        add(card);
+
+        JLabel balance = new JLabel();
+        balance .setBounds(20,400,300,20);
+        add(balance);
+
+        exit = new JButton("Exit");
+        exit.setBounds(355,520,150,30);
+        exit.addActionListener(this);
+        add(exit);
+
+        try{
+            Conn conn = new Conn();
+            ResultSet rs = conn.s.executeQuery("select * from login where pin ='"+pinnumber+"'");
+            while(rs.next()){
+                card.setText("Card Number: " +rs.getString("cardnumber").substring(0,4)+"XXXXXXXX"+rs.getString("cardnumber").substring(12));
+            }
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
+        try{
+            Conn conn = new Conn();
+            int bal =0;
+            ResultSet rs = conn.s.executeQuery("select * from bank where pin ='"+pinnumber+"'");
+            while(rs.next()){
+                mini.setText(mini.getText() + "<html>" + rs.getString("date") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + rs.getString("type") + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + rs.getString("amount") + "<br><br><html>");
+                if (rs.getString("type").equals("Deposit")) {
+                    bal += Integer.parseInt(rs.getString("amount"));
+                } else {
+                    bal -= Integer.parseInt(rs.getString("amount"));
+                }
+            }
+            balance.setText("Your account balance is Rs " + bal);
+        } catch(Exception e){
+            System.out.println(e);
+        }
+
+        mini.setBounds(20,140,400,200);
+
+        setSize(600,600);
+        setLocation(20,20);
+        getContentPane().setBackground(Color.WHITE);
+        setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource()==exit){
+            setVisible(false);
+            new Transactions(pinnumber).setVisible(true);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new MiniStatement("");
+    }
+}
